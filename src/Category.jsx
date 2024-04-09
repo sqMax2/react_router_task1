d4d0190a65ed33d3894932f600e8b953cc5023f5
Sergei <msvp@mail.ru>
2024-04-09 18:06:31 +0300

    feat: Implements data fetching and category elements mapping to <li/>

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,14 +1,29 @@
-export default function Category({link, displayField, navField, ...rest}) {
-	
-	fetch(link)
-  
-	  .then((response) => response.json())
-	  .then((obj) => console.log(obj));
-  
-	return (
-	  <ul>
+import { useState } from "react";
+import useDataFetching from "./useDataFetching";
+import { useEffect } from "react";
 
-	  </ul>
-	);
-  }
-  
+export default function Category({ link, displayField, navField, ...rest }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(useDataFetching(link));
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item[navField]}>
+              <img src={item["image"]} alt="" />
+              {item[displayField]}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="alert-text">Loading...</div>
+      )}
+      ;
+    </>
+  );
+}
