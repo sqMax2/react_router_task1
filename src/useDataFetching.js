bff2cd0293be81c03c3a3d26d1c99dc16f29a6c4
Sergei <msvp@mail.ru>
2024-04-09 18:05:37 +0300

    feat: Adds useDataFething hook

---
+++ src/useDataFetching.js
@@ -0,0 +1,19 @@
+import { useState, useEffect, useMemo } from 'react';
+
+export default function useDataFetching(url){
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, [url]);
+
+  const memoizedData = useMemo(() => data, [data]);
+
+  return memoizedData;
+}
