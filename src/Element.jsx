a519c10565cc23a4e46db2f20722b9e13ae8b550
Sergei <msvp@mail.ru>
2024-11-08 18:52:46 +0300

    feat: Adds element data display

--- src/Element.jsx
+++ src/Element.jsx
@@ -1,15 +1,31 @@
 import { useParams, useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
+import { useMemo, useState } from "react";
+import useDataFetching from "./useDataFetching";
 
 export default function Element({link, navField, ...rest}) {
-
+	const [data, setData] = useState([]);
+	const dataFetched = useDataFetching(link);
 	const params = useParams();
-
+	
+	useEffect(() => {
+		setData(dataFetched?dataFetched.find((element) => element.id == params.id):null);
+	  }, [dataFetched]);
+	
+	  const memoizedData = useMemo(() => data, [data]);
+	
 	return (
-	  <div>
-		Element {link} {navField} {params.id}
-
-	  </div>
+		<>
+			{memoizedData ? 
+				Object.entries(memoizedData).map(([key, value]) => (
+					<p>
+						{key == "image"?<img src={value} alt="" />: key == 'id'?'':`${key}: ${value?value:'none'}`}
+					</p>
+					))
+			 : (
+				<div className="alert-text">Loading...</div>
+			)}
+		</>
 	);
   }
   
