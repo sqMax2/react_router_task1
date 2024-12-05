f1438329cc3fbe61563fef0faf4f61601af11223
Sergei <msvp@mail.ru>
2024-12-05 18:42:28 +0300

    feat: Adds 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
             <Route path=":id" element={<Location />} />
             <Route index element={<Locations />} />
           </Route>
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
