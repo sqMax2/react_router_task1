3baea4d2d699f63a5ffc58a3cccd7810c4f83664
Sergei <msvp@mail.ru>
2025-03-11 22:44:06 +0300

    fix: Adds check of wrong id parameters

--- src/Element.jsx
+++ src/Element.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import useDataFetching from "./useDataFetching";
@@ -7,12 +7,15 @@ export default function Element({link, navField, ...rest}) {
 	const [data, setData] = useState([]);
 	const dataFetched = useDataFetching(link);
 	const params = useParams();
+	const navigate = useNavigate();
 	
 	useEffect(() => {
-		setData(dataFetched?dataFetched.find((element) => parseInt(element.id) === parseInt(params.id)):null);
+		const tempData = Array.isArray(dataFetched)?dataFetched.find((element) => Number(element.id) === Number(params.id)):null;
+		console.log(tempData)
+		if (typeof(tempData) === "undefined") navigate('/404');
+		setData(tempData);
 	  }, [dataFetched, params]);
-	
-	
+		
 	return (
 		<>
 			{data ? 
